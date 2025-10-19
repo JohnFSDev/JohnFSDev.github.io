85355991c4031ae739c089e620ba7f6725d3c752
JohnFSDev <johnluis22@outlook.com>
2025-10-19 14:55:59 -0400

    update for vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
   images: {
     unoptimized: true,
     qualities: [75, 90, 95],
