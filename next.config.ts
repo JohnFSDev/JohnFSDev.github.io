95290812cba754fa2eb0b4be3960f6f3b5e5b9a3
JohnFSDev <johnluis22@outlook.com>
2025-10-19 13:55:23 -0400

    chore: clean previous build for correct static export

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     qualities: [75, 90, 95],
   },
-  basePath: "/portfolio",
+  ...(isProd && {
+    basePath: "/portfolio",
+    assetPrefix: "/portfolio/",
+  }),
 };
 
 export default nextConfig;
