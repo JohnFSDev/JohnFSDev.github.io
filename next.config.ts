b698937a38c96fcf6a5919dfa17a083b9ddf7bd1
JohnFSDev <johnluis22@outlook.com>
2025-10-19 14:28:47 -0400

    fix: new repository name, some config updates.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
@@ -9,10 +7,6 @@ const nextConfig: NextConfig = {
     unoptimized: true,
     qualities: [75, 90, 95],
   },
-  ...(isProd && {
-    basePath: "/portfolio",
-    assetPrefix: "/portfolio/",
-  }),
 };
 
 export default nextConfig;
